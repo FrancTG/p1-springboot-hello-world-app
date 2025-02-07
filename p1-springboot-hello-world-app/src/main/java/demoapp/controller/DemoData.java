77399955f679b3742ef11bd8c9f8181f21eb55b4
Franc Tauste Gonzalez <franctauste2000@gmail.com>
2025-02-07 11:27:35 +0100

    Nuevas funcionalidades

---
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/DemoData.java
@@ -0,0 +1,50 @@
+package demoapp.controller;
+
+public class DemoData {
+
+    private String palindrome;
+    private int evenNum;
+    private double sqrt;
+    private double sqrtChk;
+    private String calculator;
+
+    public String getPalindrome() {
+        return palindrome;
+    }
+
+    public void setPalindrome(String palindrome) {
+        this.palindrome = palindrome;
+    }
+
+    public int getEvenNum() {
+        return evenNum;
+    }
+
+    public void setEvenNum(int evenNum) {
+        this.evenNum = evenNum;
+    }
+
+    public double getSqrt() {
+        return sqrt;
+    }
+
+    public void setSqrt(double sqrt) {
+        this.sqrt = sqrt;
+    }
+
+    public double getSqrtChk() {
+        return sqrtChk;
+    }
+
+    public void setSqrtChk(double sqrtChk) {
+        this.sqrtChk = sqrtChk;
+    }
+
+    public String getCalculator() {
+        return calculator;
+    }
+
+    public void setCalculator(String calculator) {
+        this.calculator = calculator;
+    }
+}
